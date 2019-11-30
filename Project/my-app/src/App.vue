23f1d3a94897881fcda6e67775afd96e0ff91c80
SeherKhan <seherk92@gmail.com>
2019-11-29 23:14:08 -0800

    cover page designed

--- Project/my-app/src/App.vue
+++ Project/my-app/src/App.vue
@@ -14,19 +14,21 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color:#2c3e50;
 }
 
 #nav {
   padding: 30px;
+  color: white;
+  background-color: #2c3e50
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #DCDDE2;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #CC6366;
 }
 </style>
