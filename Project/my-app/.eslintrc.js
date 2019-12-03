d5a56cd178d139e0a8bb8a535d1ba40ff0632127
SeherKhan <seherk92@gmail.com>
2019-12-02 21:28:43 -0800

    rules for errors turned off

--- Project/my-app/.eslintrc.js
+++ Project/my-app/.eslintrc.js
@@ -8,10 +8,12 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off' //,
-    //'no-undef': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': false ? 'error' : 'off',
+    'no-debugger': false ? 'error' : 'off',
+    'no-unused-vars': false ? 'error' : 'off'
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
