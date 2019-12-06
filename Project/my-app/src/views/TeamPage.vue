a49f6418473d8701442cf794870b587dfbb62200
SeherKhan <seherk92@gmail.com>
2019-12-06 04:19:07 -0800

    line breaks and github link

--- Project/my-app/src/views/TeamPage.vue
+++ Project/my-app/src/views/TeamPage.vue
@@ -16,13 +16,17 @@
       </div>
       <br/><br/>
         <h1 class="display-4" style="font-size:25px;font-weight:bold;text-align:center">Demo Video</h1>
+        <br/>
         <iframe allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" frameborder="0" height="315" src="https://www.youtube.com/embed/9LY0rr_nhG0" width="560"/>
         <br/><br/><br/><br/>
         <h1 class="display-4" style="font-size:25px;font-weight:bold;text-align:center">Conference Paper</h1>
+        <br/>
         <object data="paper.pdf" height="600px" type="application/pdf" width="70%"/>
         <p>The paper can be found <a href="paper.pdf">here</a>.</p>
-        
-
+        <br/><br/>
+        <h1 class="display-4" style="font-size:25px;font-weight:bold;text-align:center">Source Code</h1>
+        <br/>
+        <a class="btn btn-outline-dark" href="https://github.com/INF554/a5-group1/" role="button">GitHub Repo</a>
     </div>
     <br/><br/><br/>
 </div>
