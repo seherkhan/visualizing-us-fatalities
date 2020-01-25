1e6ce5f27a9e13f2aef55f90ab720b5615dd4098
SeherKhan <seherk92@gmail.com>
2020-01-24 20:16:30 -0800

    individual git added, links to private repo rm

--- Project/my-app/src/views/TeamPage.vue
+++ Project/my-app/src/views/TeamPage.vue
@@ -7,11 +7,11 @@
       <div class="row">
         <div class="col-lg-6">
             <img width="150px" height="150px" src="shraddha.jpg" style="border-radius:10%;margin-top:10px;">
-            <p><a href="https://www.linkedin.com/in/shraddha-k27/">Shraddha Kulkarni</a><br>shraddhak@usc.edu<br>(Team Member)</p>
+            <p><a href="https://www.linkedin.com/in/shraddha-k27/">Shraddha Kulkarni</a><br>shraddhak@usc.edu | <a href="https://github.com/Shraddha2702"><img src="GitHub-Mark-32px.png" alt="githublink1" width="17px" height="17px" align="middle"></a><br>(Team Member)</p>
           </div>
           <div class="col-lg-6">
             <img width="150px" height="150px" src="seher.jpg" style="border-radius:10%;margin-top:10px;">
-            <p><a href="https://www.linkedin.com/in/khanseher/">Seher Khan</a><br >seherkha@usc.edu<br>(Team Member)</p>
+            <p><a href="https://www.linkedin.com/in/khanseher/">Seher Khan</a><br >seherkha@usc.edu | <a href="https://github.com/seherkhan"><img src="GitHub-Mark-32px.png" alt="githublink1" width="17px" height="17px" align="middle"></a><br>(Team Member)</p>
           </div>
       </div>
       <br/><br/>
@@ -24,9 +24,9 @@
         <object data="paper.pdf" height="600px" type="application/pdf" width="70%"/>
         <p>The paper can be found <a href="paper.pdf">here</a>.</p>
         <br/><br/>
-        <h1 class="display-4" style="font-size:25px;font-weight:bold;text-align:center">Source Code</h1>
+        <!--h1 class="display-4" style="font-size:25px;font-weight:bold;text-align:center">Source Code</h1>
         <br/>
-        <a class="btn btn-outline-dark" href="https://github.com/INF554/a5-group1/" role="button">GitHub Repo</a>
+        <a class="btn btn-outline-dark" href="https://github.com/INF554/a5-group1/" role="button">GitHub Repo</a-->
     </div>
     <br/><br/><br/>
 </div>
